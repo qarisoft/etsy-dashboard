e2b31d24c3b1d4a8ecf171bb056cc63a2f0a9030
qarisoft <131317208+qarisoft@users.noreply.github.com>
2025-01-08 06:48:06 +0200

    first3

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output:'./out'
+   output: "export",
+   basePath: "/etsy-dashboard",
   /* config options here */
 };
 
